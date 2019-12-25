0fe858022af0316564f517d679ae08e96aeb7b44
Alexandr Rodik <alexandr.rodik@gmail.com>
2019-12-25 23:09:47 +0200

    add metadata to run app as CLI

--- src/cli.js
+++ src/cli.js
@@ -2,7 +2,7 @@ const {startCommand} = require("./commands/start");
 
 async function runCli() {
     const yargs = require('yargs')
-        .scriptName("mindful")
+        .scriptName("mindful-timer")
         .usage('$0 <cmd> [args]')
         .command(startCommand.signature, startCommand.description, startCommand.configure, startCommand.run)
         .help()
